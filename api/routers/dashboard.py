beee056bde4868d0a2dd42f5488406a3b70c611a
santifogante06 <santiagofogante@gmail.com>
2025-07-26 23:06:13 -0300

    Filtros para busquedas por query parameters

--- api/routers/dashboard.py
+++ api/routers/dashboard.py
@@ -5,6 +5,7 @@ from fastapi import Depends
 from backend.utils.load_queries import load_queries
 from typing import List
 from fastapi import HTTPException, status
+from fastapi import Query
 from api.models.models import (
     ProductStock,
     LocationStock,
@@ -18,10 +19,39 @@ router = APIRouter()
 queries = load_queries("api/queries_api.sql")
 
 @router.get("/product-stock", response_model=List[ProductStock])
-async def product_stock(db: MySQLConnection = Depends(get_db_connection)):
+async def product_stock(
+    db: MySQLConnection = Depends(get_db_connection),
+    name: str | None = Query(default=None, description="Filter by product name"),
+    low_stock: bool  = Query(default=False, description="Filter by low stock status"),
+    sort_by: str | None = Query(default=None, description="Sort by column"),
+    order: str = Query(default="asc", description="Sort order (asc/desc)"),
+    limit: int = Query(default=100, description="Limit the number of results"),
+    offset: int | None = Query(default=None, description="Offset for pagination")
+):
     try:
         cursor = db.cursor(dictionary=True)
-        cursor.execute(queries["product_stock"])
+
+        base_query = queries["product_stock"]
+        conditions = []
+        parameters = []
+        if name:
+            conditions.append("product_name LIKE %s")
+            parameters.append(f"%{name}%")
+        if low_stock:
+            conditions.append("stock_quantity < 2")
+        allowed_sort_columns = ["product_name", "stock_quantity", "product_id"]
+        if sort_by in allowed_sort_columns:
+            base_query += f" ORDER BY {sort_by} {order.upper() if order in ['asc', 'desc'] else 'ASC'}"
+        if conditions:
+            base_query += " WHERE " + " AND ".join(conditions)
+        if limit:
+            base_query += " LIMIT %s"
+            parameters.append(limit)
+            if offset:
+                base_query += " OFFSET %s"
+                parameters.append(offset)
+
+        cursor.execute(base_query, parameters)
         result = cursor.fetchall()
         cursor.close()
         if not result:
@@ -33,10 +63,35 @@ async def product_stock(db: MySQLConnection = Depends(get_db_connection)):
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Error trying to fetch product stock data: {str(e)}")
 
 @router.get("/location-stock", response_model=List[LocationStock])
-async def location_stock(db: MySQLConnection = Depends(get_db_connection)):
+async def location_stock(
+    db: MySQLConnection = Depends(get_db_connection),
+    name: str | None = Query(default=None, description="Filter by product name"),
+    limit: int = Query(default=100, description="Limit the number of results"),
+    offset: int | None = Query(default=None, description="Offset for pagination"),
+    location: str | None = Query(default=None, description="Filter by location name")
+):
     try:
         cursor = db.cursor(dictionary=True)
-        cursor.execute(queries["location_stock"])
+
+        base_query = queries["location_stock"]
+        conditions = []
+        parameters = []
+        if name:
+            conditions.append("product_name LIKE %s")
+            parameters.append(f"%{name}%")
+        if location:
+            conditions.append("ubicaciones_location LIKE %s")
+            parameters.append(f"%{location}%")
+        if conditions:
+            base_query += " WHERE " + " AND ".join(conditions)
+        if limit:
+            base_query += " LIMIT %s"
+            parameters.append(limit)
+            if offset:
+                base_query += " OFFSET %s"
+                parameters.append(offset)
+
+        cursor.execute(base_query, parameters)
         result = cursor.fetchall()
         cursor.close()
         if not result:
@@ -48,10 +103,39 @@ async def location_stock(db: MySQLConnection = Depends(get_db_connection)):
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Error trying to fetch location stock data: {str(e)}")
 
 @router.get("/product-detail", response_model=List[ProductDetail])
-async def product_detail(db: MySQLConnection = Depends(get_db_connection)):
+async def product_detail(
+    db: MySQLConnection = Depends(get_db_connection),
+    name: str | None = Query(default=None, description="Filter by product name"),
+    limit: int = Query(default=100, description="Limit the number of results"),
+    offset: int | None = Query(default=None, description="Offset for pagination"),
+    location: str | None = Query(default=None, description="Filter by location name"),
+    sort_by: str | None = Query(default=None, description="Sort by column"),
+    order: str = Query(default="asc", description="Sort order (asc/desc)")
+):
     try:
         cursor = db.cursor(dictionary=True)
-        cursor.execute(queries["product_detail"])
+        base_query = queries["product_detail"]
+        conditions = []
+        parameters = []
+        if name:
+            conditions.append("product_name LIKE %s")
+            parameters.append(f"%{name}%")
+        if location:
+            conditions.append("ubicaciones_location LIKE %s")
+            parameters.append(f"%{location}%")
+        allowed_sort_columns = ["product_name", "stock_quantity", "ubicaciones_location"]
+        if sort_by in allowed_sort_columns:
+            base_query += f" ORDER BY {sort_by} {order.upper() if order in ['asc', 'desc'] else 'ASC'}"
+        if conditions:
+            base_query += " WHERE " + " AND ".join(conditions)
+        if limit:
+            base_query += " LIMIT %s"
+            parameters.append(limit)
+            if offset:
+                base_query += " OFFSET %s"
+                parameters.append(offset)
+
+        cursor.execute(base_query, parameters)
         result = cursor.fetchall()
         cursor.close()
         if not result:
@@ -74,10 +158,55 @@ async def low_stock_alert(db: MySQLConnection = Depends(get_db_connection)):
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Error trying to fetch low stock alert data: {str(e)}")
 
 @router.get("/stock-movement", response_model=List[StockMovement])
-async def stock_movement(db: MySQLConnection = Depends(get_db_connection)):
+async def stock_movement(
+    db: MySQLConnection = Depends(get_db_connection),
+    name: str | None = Query(default=None, description="Filter by product name"),
+    limit: int = Query(default=100, description="Limit the number of results"),
+    offset: int | None = Query(default=None, description="Offset for pagination"),
+    location: str | None = Query(default=None, description="Filter by location name"),
+    sort_by: str | None = Query(default=None, description="Sort by column"),
+    order: str = Query(default="asc", description="Sort order (asc/desc)"),
+    action: str | None = Query(default=None, description="Filter by action type (e.g., 'insert', 'update')"),
+    action_function: str | None = Query(default=None, description="Filter by action function (e.g., 'add', 'remove')"),
+    from_date: str | None = Query(default=None, description="Filter from date"),
+    to_date: str | None = Query(default=None, description="Filter to date")
+):
     try:
         cursor = db.cursor(dictionary=True)
-        cursor.execute(queries["stock_movement"])
+        base_query = queries["stock_movement"]
+        conditions = []
+        parameters = []
+        if name:
+            conditions.append("product_name LIKE %s")
+            parameters.append(f"%{name}%")
+        if location:
+            conditions.append("ubicaciones_location LIKE %s")
+            parameters.append(f"%{location}%")
+        allowed_sort_columns = ["product_name", "stock_quantity", "ubicaciones_location"]
+        if sort_by in allowed_sort_columns:
+            base_query += f" ORDER BY {sort_by} {order.upper() if order in ['asc', 'desc'] else 'ASC'}"
+        if action in ["insert", "update"]:
+            conditions.append("action = %s")
+            parameters.append(action)
+        if action_function in ["add", "remove"]:
+            conditions.append("action_function = %s")
+            parameters.append(action_function)
+        if from_date:
+            conditions.append("h.action_date_time >= %s")
+            parameters.append(from_date)
+        if to_date:
+            conditions.append("h.action_date_time <= %s")
+            parameters.append(to_date)
+        if conditions:
+            base_query += " WHERE " + " AND ".join(conditions)
+        if limit:
+            base_query += " LIMIT %s"
+            parameters.append(limit)
+            if offset:
+                base_query += " OFFSET %s"
+                parameters.append(offset)
+
+        cursor.execute(base_query, parameters)
         result = cursor.fetchall()
         cursor.close()
         if not result:
